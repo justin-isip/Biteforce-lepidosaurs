a5766245f0d7a97f1d3a389c6ed6648a51f60705
unknown <Justin Isip@LSCI-F52LKC2.nhm.ac.uk>
2020-07-29 11:50:24 +0100

    Update script to include higher taxonomy column

--- data-wrangling/01B-join-ecological-data.R
+++ data-wrangling/01B-join-ecological-data.R
@@ -35,41 +35,41 @@ max_bf_overall <- ecologicaldata %>%
   inner_join(max_bf_overall, ecologicaldata, by = "BinomialReptileDatabase") %>%
   
   # Select the columns we want to keep
-  select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, max_bf, max_bm, max_svl, max_hl, max_hw, max_hh, max_ljl, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+  select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, max_bf, max_bm, max_svl, max_hl, max_hw, max_hh, max_ljl, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
   
 # Do the same for all other data sets
 
 max_bf_males <- ecologicaldata %>% 
 inner_join(max_bf_males, ecologicaldata, by = "BinomialReptileDatabase") %>%
-select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, max_bf, max_bm, max_svl, max_hl, max_hw, max_hh, max_ljl, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, max_bf, max_bm, max_svl, max_hl, max_hw, max_hh, max_ljl, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
 
 max_bf_females <- ecologicaldata %>% 
   inner_join(max_bf_females, ecologicaldata, by = "BinomialReptileDatabase") %>%
-  select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, max_bf, max_bm, max_svl, max_hl, max_hw, max_hh, max_ljl, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+  select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, max_bf, max_bm, max_svl, max_hl, max_hw, max_hh, max_ljl, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
 
 avg_bf_plus_sd_overall <- ecologicaldata %>% 
   inner_join(avg_bf_plus_sd_overall, ecologicaldata, by = "BinomialReptileDatabase") %>%
-  select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, bf_plus_sd, bm_plus_sd, svl_plus_sd, hl_plus_sd, hw_plus_sd, hh_plus_sd, ljl_plus_sd, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+  select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, bf_plus_sd, bm_plus_sd, svl_plus_sd, hl_plus_sd, hw_plus_sd, hh_plus_sd, ljl_plus_sd, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
 
 avg_bf_plus_sd_males <- ecologicaldata %>% 
   inner_join(avg_bf_plus_sd_males, ecologicaldata, by = "BinomialReptileDatabase") %>%
-  select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, bf_plus_sd, bm_plus_sd, svl_plus_sd, hl_plus_sd, hw_plus_sd, hh_plus_sd, ljl_plus_sd, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+  select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, bf_plus_sd, bm_plus_sd, svl_plus_sd, hl_plus_sd, hw_plus_sd, hh_plus_sd, ljl_plus_sd, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
 
 avg_bf_plus_sd_females <- ecologicaldata %>% 
   inner_join(avg_bf_plus_sd_females, ecologicaldata, by = "BinomialReptileDatabase") %>%
-  select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, bf_plus_sd, bm_plus_sd, svl_plus_sd, hl_plus_sd, hw_plus_sd, hh_plus_sd, ljl_plus_sd, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+  select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, bf_plus_sd, bm_plus_sd, svl_plus_sd, hl_plus_sd, hw_plus_sd, hh_plus_sd, ljl_plus_sd, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
 
 avg_bf_plus_se_overall <- ecologicaldata %>% 
   inner_join(avg_bf_plus_se_overall, ecologicaldata, by = "BinomialReptileDatabase") %>%
-  select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, bf_plus_se, bm_plus_se, svl_plus_se, hl_plus_se, hw_plus_se, hh_plus_se, ljl_plus_se, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+  select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, bf_plus_se, bm_plus_se, svl_plus_se, hl_plus_se, hw_plus_se, hh_plus_se, ljl_plus_se, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
 
 avg_bf_plus_se_males <- ecologicaldata %>% 
   inner_join(avg_bf_plus_se_males, ecologicaldata, by = "BinomialReptileDatabase") %>%
-  select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, bf_plus_se, bm_plus_se, svl_plus_se, hl_plus_se, hw_plus_se, hh_plus_se, ljl_plus_se, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+  select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, bf_plus_se, bm_plus_se, svl_plus_se, hl_plus_se, hw_plus_se, hh_plus_se, ljl_plus_se, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
 
 avg_bf_plus_se_females <- ecologicaldata %>% 
   inner_join(avg_bf_plus_se_females, ecologicaldata, by = "BinomialReptileDatabase") %>%
-  select(BinomialReptileDatabase, Sex, Family, SSM, SSBF, bf_plus_se, bm_plus_se, svl_plus_se, hl_plus_se, hw_plus_se, hh_plus_se, ljl_plus_se, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
+  select(BinomialReptileDatabase, Sex, Family, HigherTaxonomy, SSM, SSBF, bf_plus_se, bm_plus_se, svl_plus_se, hl_plus_se, hw_plus_se, hh_plus_se, ljl_plus_se, MainBiogeographicRealm, Lifestyle, MieriDiet, MetzgerHerrelDiet, CooperDiet, PercentPlantMatterEaten)
 
 
 # Now that I've merged the ecological data with the tidied data for all datasets, rewrite them all to csv 
